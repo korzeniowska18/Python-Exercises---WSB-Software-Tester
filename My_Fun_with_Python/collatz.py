609b68722bca21b9a8ed92955df94b6759e6ab6a
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-06-05 13:26:58 +0200

    add to file

--- My_Fun_with_Python/collatz.py
+++ My_Fun_with_Python/collatz.py
@@ -15,3 +15,23 @@ def collatz(number):
 
 
 collatz(inputNumber)
+
+"""
+print("This is Collatz program")
+
+inputNumber = int(input("Podaj liczbę całkowitą: "))
+
+def collatz(number):
+    print(number)
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+
+        elif number % 2 == 1:
+            number = number *3 + 1
+            print(number)
+
+
+collatz(inputNumber)
+"""
