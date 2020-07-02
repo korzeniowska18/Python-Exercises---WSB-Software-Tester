e8a46ceb8218a9fb5aff9483978defa45055d07d
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2020-07-02 06:49:52 +0200

    create file

---
+++ My_Fun_with_Python/znaki_sterujace.py
@@ -0,0 +1,22 @@
+"""
+\' = Apostrof
+\" = Cudzysłów
+\t = Tabulator
+\n = Nowy wiersz(Enter)
+\\ = Ukośnik
+"""
+
+print('Jest to cytat:\n \n \"Jedynym sposobem na prawdziwą satysfakcję z pracy jest robienie tego,\
+ w czego wielkość się wierzy. \n A jedynym sposobem na robienie rzeczy wielkich jest miłość do tego, co się robi.\"\n \n \t \t \\Steve Jobs\\')
+
+"""
+wynik:
+
+>>>
+Jest to cytat:
+ 
+ "Jedynym sposobem na prawdziwą satysfakcję z pracy jest robienie tego,w czego wielkość się wierzy. 
+ A jedynym sposobem na robienie rzeczy wielkich jest miłość do tego, co się robi."
+ 
+ 	 	 \Steve Jobs\
+>>> 
