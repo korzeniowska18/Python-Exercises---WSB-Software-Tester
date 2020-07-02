66e3aa2e9d7a37e3d66aaf3d996334a88f910fa2
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2020-07-02 06:59:27 +0200

    update

--- My_Fun_with_Python/znaki_sterujace.py
+++ My_Fun_with_Python/znaki_sterujace.py
@@ -4,6 +4,7 @@
 \t = Tabulator
 \n = Nowy wiersz(Enter)
 \\ = Ukośnik
+''' = potrójny apostrof
 
 "r" przed ciągiem tekstowym "raw string" oznacza niezmodyfikowany ciąg tekstowy i wypisuje wszystkie znaki sterujące
 """
@@ -35,4 +36,24 @@ Jest to cytat:\n \n \"Jedynym sposobem na prawdziwą satysfakcję z pracy jest r
  w czego wielkość się wierzy. \n A jedynym sposobem na robienie rzeczy wielkich jest miłość do tego, co się robi.\"\n \n \t \t \\Steve Jobs\\
 
 >>>
+"""
+ print('''Jest to cytat:
+
+"Jedynym sposobem na prawdziwą satysfakcję z pracy jest robienie tego, w czego wielkość się wierzy.
+A jedynym sposobem na robienie rzeczy wielkich jest miłość do tego, co się robi."
+
+          \Steve Jobs\ ''')
+ 
+ """
+ wynik:
+ 
+ >>>
+ 
+ Jest to cytat:
+
+"Jedynym sposobem na prawdziwą satysfakcję z pracy jest robienie tego, w czego wielkość się wierzy.
+A jedynym sposobem na robienie rzeczy wielkich jest miłość do tego, co się robi."
+
+          \Steve Jobs\ 
+>>> 
 """
