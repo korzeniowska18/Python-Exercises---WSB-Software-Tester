1d52a991634381a4bb3d41ccc901c908d4580193
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2020-08-05 06:51:56 +0200

    create a file

---
+++ My_Fun_with_Python/lambda.py
@@ -0,0 +1,22 @@
+```
+>>> suma = lambda x, y: x + y
+>>> suma(5, 4)
+9
+>>> (lambda x: x * x)(3)
+9
+>>> (lambda x:(x % 2 and 'yes' or 'no'))(3)
+'yes'
+
+>>> (lambda x, y, z: x + y + z)(1, 2, 3)
+6
+>>> (lambda x, y, z=3: x + y + z)(1, 2)
+6
+>>> (lambda x, y, z=3: x + y + z)(1, y=2)
+6
+>>> (lambda *args: sum(args))(1,2,3)
+6
+>>> (lambda **kwargs: sum(kwargs.values()))(one=1, two=2, three=3)
+6
+>>> (lambda x, *, y=0, z=0: x + y + z)(1, y=2, z=3)
+6
+```
