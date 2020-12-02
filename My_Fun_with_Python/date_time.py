aff0507e7890e1c860eca9bf2da153c42ba63398
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2020-12-02 08:26:59 +0100

    update

--- My_Fun_with_Python/date_time.py
+++ My_Fun_with_Python/date_time.py
@@ -7,9 +7,10 @@ from datetime import time
 def main():
     obecny_czas = datetime.now()
     print(obecny_czas.strftime("Obecnie jest %Y rok"))
+    print(obecny_czas.strftime("Dziś jest: %A, %d %B %Y"))
 
 if __name__ == "__main__":
     main()
 
 >>> Obecnie jest 2020 rok
-
+>>> Dziś jest: Wednesday, 02 December 2020
