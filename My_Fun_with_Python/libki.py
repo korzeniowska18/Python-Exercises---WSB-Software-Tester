b7edc61ca8ba2872b5b55d4d639c1372c5775e4b
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-13 09:21:56 +0100

    update

--- My_Fun_with_Python/libki.py
+++ My_Fun_with_Python/libki.py
@@ -1,3 +1,12 @@
 import os
 os.getcwd()   -> wskazuję ścieżkę do aktualnego folderu w którym się znajdujesz
 
+import keyword
+keyword.kwlist
+print(keyword.kwlist)   => jakich słów nie należy używać w nazewnictwie
+
+>>>
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 
+ 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
+ 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>>
