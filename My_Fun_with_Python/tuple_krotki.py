c39906bbceb76ad388fcb42e1c29241fdeb94adc
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-15 15:59:00 +0100

    update

--- My_Fun_with_Python/tuple_krotki.py
+++ My_Fun_with_Python/tuple_krotki.py
@@ -47,3 +47,46 @@ Ford 2015 red 1 owner
 >>> print(a, b)
 18 14
 >>> 
+>>> x = (55, 3, 6, 1, 9, 11)
+>>> x[-1]
+11
+>>> x.index(55)
+0
+>>> x.count(11)
+1
+>>> max(x)
+55
+>>> x
+(55, 3, 6, 1, 9, 11)
+>>> list(x)
+[55, 3, 6, 1, 9, 11]
+>>> x
+(55, 3, 6, 1, 9, 11)
+>>> x.append(50)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    x.append(50)
+AttributeError: 'tuple' object has no attribute 'append'
+>>> y = list(x)
+>>> y
+[55, 3, 6, 1, 9, 11]
+>>> y.append(50)
+>>> y
+[55, 3, 6, 1, 9, 11, 50]
+>>> x = tuple(y)
+>>> x
+(55, 3, 6, 1, 9, 11, 50)
+>>> (g1, g2, g3, g4) = x
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    (g1, g2, g3, g4) = x
+ValueError: too many values to unpack (expected 4)
+>>> d = 1
+>>> c =10
+>>> print("d =",d,"\tc",c)
+d = 1 	c 10
+>>> print("d =",d,"\tc =",c)
+d = 1 	c = 10
+>>> (d,c) = (c,d)
+>>> print("d =",d,"\tc =",c)
+d = 10 	c = 1
