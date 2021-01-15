a4c714c5dcbd566705a1e75b988c5527994925dd
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-15 15:35:40 +0100

    update

--- My_Fun_with_Python/listy.py
+++ My_Fun_with_Python/listy.py
@@ -52,3 +52,104 @@
 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
 >>> 
 
+>>> a = [1, 3, 5, 7, 7, 3, 2, 'apple', 'orange']
+>>> a[1] = 10
+>>> a
+[1, 10, 5, 7, 7, 3, 2, 'apple', 'orange']
+>>> a.append('fruits')
+>>> a
+[1, 10, 5, 7, 7, 3, 2, 'apple', 'orange', 'fruits']
+>>> a.insert(6, 22)
+>>> a
+[1, 10, 5, 7, 7, 3, 22, 2, 'apple', 'orange', 'fruits']
+>>> a.remove([3])
+>>> a.remove(7)
+>>> a
+[1, 10, 5, 7, 3, 22, 2, 'apple', 'orange', 'fruits']
+
+>>> a
+[1, 2, 3, 5, 7, 10, 22, 'apple', 'orange', 'fruits']
+>>> a
+[1, 2, 3, 5, 7, 10, 22, 'apple', 'orange', 'fruits']
+>>> a
+[1, 2, 3, 5, 7, 10, 22, 'apple', 'orange', 'fruits']
+>>> a
+[1, 2, 3, 5, 7, 10, 22, 'apple', 'orange', 'fruits']
+a
+>>> 
+>>> a
+[1, 2, 3, 5, 7, 10, 22, 'apple', 'orange', 'fruits']
+>>> a.sort()
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    a.sort()
+TypeError: '<' not supported between instances of 'str' and 'int'
+>>> a = 'apple', 'orange', 'fruits']
+SyntaxError: unmatched ']'
+>>> a.sort()
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    a.sort()
+TypeError: '<' not supported between instances of 'str' and 'int'
+>>> a = ['apple', 'orange', 'fruits']
+>>> a.sort()
+>>> a
+['apple', 'fruits', 'orange']
+>>> b = [1, 2, 3, 5, 7, 10, 22]
+>>> b.sort()
+>>> b
+[1, 2, 3, 5, 7, 10, 22]
+>>> sorted(b)
+[1, 2, 3, 5, 7, 10, 22]
+>>> b
+[1, 2, 3, 5, 7, 10, 22]
+>>> b.reverse()
+>>> b
+[22, 10, 7, 5, 3, 2, 1]
+>>> b
+[22, 10, 7, 5, 3, 2, 1]
+>>> sorted(b)
+[1, 2, 3, 5, 7, 10, 22]
+>>> b
+[22, 10, 7, 5, 3, 2, 1]
+>>> a.index('apple')
+0
+>>> a.append(10)
+>>> a
+['apple', 'fruits', 'orange', 10]
+>>> b.append(10)
+>>> b
+[22, 10, 7, 5, 3, 2, 1, 10]
+>>> b.count(10)
+2
+>>> a.extend(b)
+>>> a
+['apple', 'fruits', 'orange', 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> a = b
+>>> a
+[22, 10, 7, 5, 3, 2, 1, 10]
+>>> b
+[22, 10, 7, 5, 3, 2, 1, 10]
+>>> a.extend(b)
+>>> a
+[22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> b = a
+>>> a
+[22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> b
+[22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> b.clear()
+>>> b
+[]
+>>> a
+[]
+>>> a = [22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> b = a.copy()
+>>> b
+[22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+>>> b.clear()
+>>> b
+[]
+>>> a
+[22, 10, 7, 5, 3, 2, 1, 10, 22, 10, 7, 5, 3, 2, 1, 10]
+
