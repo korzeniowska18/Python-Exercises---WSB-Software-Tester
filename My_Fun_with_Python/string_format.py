9440b9058d2c919c600ef58f9ddb40a8af5e713c
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-15 14:20:37 +0100

    update

--- My_Fun_with_Python/string_format.py
+++ My_Fun_with_Python/string_format.py
@@ -33,3 +33,16 @@ line = '{0:s} {1:d}'
 print(line.format('Lody', 3))
 print(line.format('Wycieczka', 119))
 print(line.format('PIZZA Margarita', 6))
+
+name = 'Jan'
+age = 26
+daysInYear = 365
+message = '%s is %d years old, so is about %d days old'
+print(message % (name,age,age*daysInYear))
+name = 'Chris'
+age = 17
+print(message % (name,age,age*daysInYear))
+message = '{0:s} is {1:d} years old, so is about {2:d} days old'
+print(message.format(name,age,age*daysInYear))
+print('1234567890 divided by 12345 is ',1234567890 // 12345,'and the rest is',1234567890 % 12345)
+
