dfae47f16797df4db6389c691193e51b363fd37c
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-15 16:59:04 +0100

    create file

---
+++ My_Fun_with_Python/instrulcja_warunkowa_if.py
@@ -0,0 +1,111 @@
+wiek = 19
+
+if wiek >=18:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie")
+
+OdpowiedniStroj = True
+
+if wiek >=18 and OdpowiedniStroj:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj")
+
+InneOgraniczenia = False
+
+if wiek >=18 and OdpowiedniStroj and not InneOgraniczenia:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie \
+i posiadające odpowiedni stroj z brakiem innych ograniczeń")
+
+>>>
+Możesz brać udział w zawodach
+Możesz brać udział w zawodach
+Możesz brać udział w zawodach
+>>> 
+
+wiek = 17
+
+if wiek >=18:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie")
+
+OdpowiedniStroj = True
+
+if wiek >=18 and OdpowiedniStroj:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj")
+
+InneOgraniczenia = False
+
+if wiek >=18 and OdpowiedniStroj and not InneOgraniczenia:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie \
+i posiadające odpowiedni stroj z brakiem innych ograniczeń")
+
+>>>
+W zawodach mogą brac udział tylko osoby pełnoletnie
+W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj
+W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj z brakiem innych ograniczeń
+>>>
+
+wiek = 20
+
+if wiek >=18:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie")
+
+OdpowiedniStroj = False
+
+if wiek >=18 and OdpowiedniStroj:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj")
+
+InneOgraniczenia = False
+
+if wiek >=18 and OdpowiedniStroj and not InneOgraniczenia:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie \
+i posiadające odpowiedni stroj z brakiem innych ograniczeń")
+
+>>>
+Możesz brać udział w zawodach
+W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj
+W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj z brakiem innych ograniczeń
+>>>
+
+wiek = 20
+
+if wiek >=18:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie")
+
+OdpowiedniStroj = True
+
+if wiek >=18 and OdpowiedniStroj:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj")
+
+InneOgraniczenia = True
+
+if wiek >=18 and OdpowiedniStroj and not InneOgraniczenia:
+    print("Możesz brać udział w zawodach")
+else:
+    print("W zawodach mogą brac udział tylko osoby pełnoletnie \
+i posiadające odpowiedni stroj z brakiem innych ograniczeń")
+
+>>>
+Możesz brać udział w zawodach
+Możesz brać udział w zawodach
+W zawodach mogą brac udział tylko osoby pełnoletnie i posiadające odpowiedni stroj z brakiem innych ograniczeń
+>>>
