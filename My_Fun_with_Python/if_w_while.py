2a481f5579ec2432c2f10d2e9ea4957f08ac5c31
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-18 10:29:47 +0100

    update

--- My_Fun_with_Python/if_w_while.py
+++ My_Fun_with_Python/if_w_while.py
@@ -252,5 +252,69 @@ Zawartość kontenera:  [45, 40, 5]
 Liczba paczek w kontenerze:  3
 >>>
 
+#sumy dwóch kolejnych liczb od 0 do 50
+previous_number = 0
+ 
+while number<=50:
+    print(number + previous_number)
+    previous_number=number
+    number+=1
+
+>>>
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+81
+83
+85
+87
+89
+91
+93
+95
+97
+99
+>>> 
+#Komputer wymyśli sobie liczbę od 0 do 20, a Ty musisz ją zgadnąć!
+
+
+
 
 
