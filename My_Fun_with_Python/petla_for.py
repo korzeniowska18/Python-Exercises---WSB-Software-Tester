5255abdda85438b48ec87ca4834bd34a8425914c
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-19 10:02:33 +0100

    update

--- My_Fun_with_Python/petla_for.py
+++ My_Fun_with_Python/petla_for.py
@@ -58,3 +58,59 @@ Internet connection lost
 ERROR
 Access denied
 >>> 
+for liczba in range(1,16, 2):
+    print(liczba)
+    
+>>>
+1
+3
+5
+7
+9
+11
+13
+15
+>>> 
+
+for liczba in range(1,20):
+    if liczba %2 == 0:
+        print('Liczba %2d jest %s' % (liczba, 'parzysta'))
+        
+>>>
+Liczba  2 jest parzysta
+Liczba  4 jest parzysta
+Liczba  6 jest parzysta
+Liczba  8 jest parzysta
+Liczba 10 jest parzysta
+Liczba 12 jest parzysta
+Liczba 14 jest parzysta
+Liczba 16 jest parzysta
+Liczba 18 jest parzysta
+>>>
+for liczba in range(1,20):
+    if liczba %2 == 0:
+        print('Liczba %2d jest %s' % (liczba, 'parzysta'))
+    #print(liczba)
+    else:
+        print('Liczba %2d jest %s' % (liczba, 'nieparzysta'))
+>>>
+Liczba  1 jest nieparzysta
+Liczba  2 jest parzysta
+Liczba  3 jest nieparzysta
+Liczba  4 jest parzysta
+Liczba  5 jest nieparzysta
+Liczba  6 jest parzysta
+Liczba  7 jest nieparzysta
+Liczba  8 jest parzysta
+Liczba  9 jest nieparzysta
+Liczba 10 jest parzysta
+Liczba 11 jest nieparzysta
+Liczba 12 jest parzysta
+Liczba 13 jest nieparzysta
+Liczba 14 jest parzysta
+Liczba 15 jest nieparzysta
+Liczba 16 jest parzysta
+Liczba 17 jest nieparzysta
+Liczba 18 jest parzysta
+Liczba 19 jest nieparzysta
+>>>
