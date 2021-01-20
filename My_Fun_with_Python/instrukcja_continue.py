927d7e8b1a1b2fa2e9513cbe0ade2249ce77f63e
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-20 07:36:07 +0100

    update

--- My_Fun_with_Python/instrukcja_continue.py
+++ My_Fun_with_Python/instrukcja_continue.py
@@ -52,3 +52,149 @@ krisitina@mojafirma.pl
 jon@mojafirma.com
 joanna@mojafirma.pl
 >>> 
+
+menu = '''
+Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+'''
+ 
+usmiech = '''
+ 
+                          oooo$$$$$$$$$$$$oooo
+                      oo$$$$$$$$$$$$$$$$$$$$$$$$o
+                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$
+   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$
+oo $ $ "$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$
+"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$
+  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$
+  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  """$$$
+   "$$$""""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$
+    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$o
+   o$$"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o
+   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" "$$$$$$ooooo$$$$o
+  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$
+  $$$$$$$$"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$""""""""
+ """"       $$$$    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"      o$$$
+            "$$$o     """$$$$$$$$$$$$$$$$$$"$$"         $$$
+              $$$o          "$$""$$$$$$""""           o$$$
+               $$$$o                                o$$$"
+                "$$$$o      o$$$$$$o"$$$$o        o$$$$
+                  "$$$$$oo     ""$$$$o$$$$$o   o$$$$""
+                     ""$$$$$oooo  "$$$o$$$$$$$$$"""
+                        ""$$$$$$$oo $$$$$$$$$$
+                                """"$$$$$$$$$$$
+                                    $$$$$$$$$$$$
+                                     $$$$$$$$$$"
+                                      "$$$""""
+'''
+
+while True:
+ 
+    print(menu)
+    wybor = input('Wprowadź twój wybór:  ')
+ 
+    if wybor == '1':
+        print("Funkcja KAWA na razie nie jest dostępna")
+        input('Naciśnij enter')
+        continue
+    
+    if wybor == '2':
+        print("Funkcja HERBATA na razie nie jest dostępna")
+        input('Naciśnij enter')
+        continue
+    
+    if wybor =='3':
+        print(usmiech)
+        input('Naciśnij enter')        
+        continue
+        
+    if wybor == '0':
+        break
+ 
+    input('Musisz dokonac prawidłowego wybory, spróbuj jeszcze raz od nowa!')
+
+  >>>
+  Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+
+Wprowadź twój wybór:  1
+Funkcja KAWA na razie nie jest dostępna
+Naciśnij enter
+
+Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+
+Wprowadź twój wybór:  2
+Funkcja HERBATA na razie nie jest dostępna
+Naciśnij enter
+
+Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+
+Wprowadź twój wybór:  
+Musisz dokonac prawidłowego wybory, spróbuj jeszcze raz od nowa!
+
+Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+
+Wprowadź twój wybór:  3
+
+ 
+                          oooo$$$$$$$$$$$$oooo
+                      oo$$$$$$$$$$$$$$$$$$$$$$$$o
+                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$
+   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$
+oo $ $ "$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$
+"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$
+  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$
+  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  """$$$
+   "$$$""""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$
+    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$o
+   o$$"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o
+   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" "$$$$$$ooooo$$$$o
+  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$
+  $$$$$$$$"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$""""""""
+ """"       $$$$    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"      o$$$
+            "$$$o     """$$$$$$$$$$$$$$$$$$"$$"         $$$
+              $$$o          "$$""$$$$$$""""           o$$$
+               $$$$o                                o$$$"
+                "$$$$o      o$$$$$$o"$$$$o        o$$$$
+                  "$$$$$oo     ""$$$$o$$$$$o   o$$$$""
+                     ""$$$$$oooo  "$$$o$$$$$$$$$"""
+                        ""$$$$$$$oo $$$$$$$$$$
+                                """"$$$$$$$$$$$
+                                    $$$$$$$$$$$$
+                                     $$$$$$$$$$"
+                                      "$$$""""
+
+Naciśnij enter
+
+Wybiezr na co masz ochotę:
+1 - KAWA
+2 - HERBATA
+3 - UŚMIECHNIJ SIĘ
+---------------
+Żeby zakończyć ten program, wybierz 0
+
+Wprowadź twój wybór:  0
+>>> 
