6439fa3a5b9108edf8ed28769da9ca15fad1579c
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-01-21 07:28:28 +0100

    update

--- My_Fun_with_Python/petla_while_else.py
+++ My_Fun_with_Python/petla_while_else.py
@@ -151,3 +151,79 @@ Dwa do potęgi  15 wynosi  32768
 Dwa do potęgi  16 wynosi  65536
 
 >>>
+wplacony_kapital = 20000
+
+procent_odsetek = 0.03
+
+max_okres_oszczedzania = 10
+
+rok = 0
+
+kapital = wplacony_kapital
+
+while rok < max_okres_oszczedzania:
+    rok +=1
+    kapital = round((1 + procent_odsetek)* kapital,2)
+    print('Po zakończeniu ', rok, 'roku na końcie będzie kwota w wysokości', kapital)
+else:
+    print('Całkowita kwota oszczędności w ciągu 10 lat wynosi ', kapital - wplacony_kapital)
+    
+>>>
+Po zakończeniu  1 roku na końcie będzie kwota w wysokości 20600.0
+Po zakończeniu  2 roku na końcie będzie kwota w wysokości 21218.0
+Po zakończeniu  3 roku na końcie będzie kwota w wysokości 21854.54
+Po zakończeniu  4 roku na końcie będzie kwota w wysokości 22510.18
+Po zakończeniu  5 roku na końcie będzie kwota w wysokości 23185.49
+Po zakończeniu  6 roku na końcie będzie kwota w wysokości 23881.05
+Po zakończeniu  7 roku na końcie będzie kwota w wysokości 24597.48
+Po zakończeniu  8 roku na końcie będzie kwota w wysokości 25335.4
+Po zakończeniu  9 roku na końcie będzie kwota w wysokości 26095.46
+Po zakończeniu  10 roku na końcie będzie kwota w wysokości 26878.32
+Całkowita kwota oszczędności w ciągu 10 lat wynosi  6878.32
+>>> 
+
+number=20730906
+tmpnumber = number
+sumOfDigits = 0
+while tmpnumber >0:
+    digit = tmpnumber % 10
+    sumOfDigits += digit
+    tmpnumber = tmpnumber//10
+else:
+    print('the sum of digits of ', number, ' is',sumOfDigits)
+    
+>>>
+the sum of digits of  20730906  is 27
+-------------------------------------------------------
+>>> 
+
+text = 'Rozwiązania oparte na VPN stosowane są np. w sieciach korporacyjnych, \
+których zdalni użytkownicy pracują ze swoich domów na niezabezpieczonych \
+łączach. Wirtualne sieci prywatne charakteryzują się dość dużą efektywnością,\
+nawet na słabych łączach (dzięki kompresji danych) oraz wysokim \
+poziomem bezpieczeństwa (ze względu na szyfrowanie). Rozwiązanie to\
+sprawdza się w firmach, których pracownicy często podróżują lub \
+korzystają z możliwości'
+
+dlugosc_slowa = 6
+krotkie_slowa = 0
+dlugie_slowa = 0
+i = 0
+
+lista_slow = text.replace('(', '').replace(')', '').replace('\n', ' ').split(' ')
+while i < len(lista_slow):
+    if len(lista_slow[i]) > dlugosc_slowa:
+        dlugie_slowa +=1
+    else:
+        krotkie_slowa +=1
+
+    i+=1
+print('Tekst zawiera łącznie',len(lista_slow), 'słów.')
+print('Słów mniejszych niż', dlugosc_slowa,'liter jest',krotkie_slowa,'.')
+print('Słów dłuższych niż',  dlugosc_slowa,'liter jest', dlugie_slowa,'.')
+
+>>>
+Tekst zawiera łącznie 55 słów.
+Słów mniejszych niż 6 liter jest 26 .
+Słów dłuższych niż 6 liter jest 29 .
+>>> 
