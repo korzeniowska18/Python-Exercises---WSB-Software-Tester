51d66a1efd98a06ab8592a4bff961197a2af6177
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-02-25 16:44:26 +0100

    update

--- My_Fun_with_Python/random_play.py
+++ My_Fun_with_Python/random_play.py
@@ -288,3 +288,88 @@ Australia
 Switzerland
 Uruguay
 >>>>
+
+import random
+ 
+min = 1
+max = 6
+ 
+dice = random.randint(min,max)
+ 
+if dice == 1:
+    print('   ')
+    print(' o ')
+    print('   ')
+elif dice == 2:
+    print('  o')
+    print('   ')
+    print('o  ')
+elif dice == 3:
+    print('  o')
+    print(' o ')
+    print('o  ')
+elif dice == 4:
+    print('o o')
+    print('   ')
+    print('o o')
+elif dice == 5:
+    print('o o')
+    print(' o ')
+    print('o o')
+else:
+    print('o o')
+    print('o o')
+    print('o o')
+ 
+print('----------------------')
+ 
+dices = []
+for i in range(5):
+    dices.append(random.randint(min,max))
+ 
+dices.sort()
+print(dices)
+
+>>>
+
+o o
+o o
+o o
+----------------------
+[2, 3, 4, 5, 6]
+>>> 
+============== RESTART:  ==============
+   
+ o 
+   
+----------------------
+[1, 5, 5, 6, 6]
+>>> 
+============== RESTART:  ==============
+o o
+   
+o o
+----------------------
+[1, 1, 2, 3, 4]
+>>> 
+==============  ==============
+o o
+ o 
+o o
+----------------------
+[2, 2, 5, 6, 6]
+>>> 
+
+import random
+
+liczby = range(33, 124)
+haslo = ""
+
+for i in range(15):
+    haslo += chr(random.choice(liczby))
+
+print("Hasło: ", haslo)
+
+>>>>
+Hasło:  JYDU)f]Y#&u{.6&
+>>> 
