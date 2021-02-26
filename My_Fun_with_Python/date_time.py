ca3c68a6091f66f185c767ff7f15706c4a80f40b
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-02-26 09:29:58 +0100

    update

--- My_Fun_with_Python/date_time.py
+++ My_Fun_with_Python/date_time.py
@@ -89,3 +89,99 @@ SyntaxError: unmatched ')'
 >>> calendar.month(2021, 7)
 '     July 2021\nSu Mo Tu We Th Fr Sa\n             1  2  3\n 4  5  6  7  8  9 10\n11 12 13 14 15 16 17\n18 19 20 21 22 23 24\n25 26 27 28 29 30 31\n'
 >>> 
+
+import time
+ 
+print(time.time())
+print(time.localtime(time.time()))
+ 
+import calendar
+ 
+print(calendar.month(2021,2))
+calendar.setfirstweekday(6)
+print(calendar.month(2021,3))
+ 
+print('2020 is leap: ', calendar.isleap(2020))
+ 
+print(calendar.calendar(2021))
+
+>>>>
+
+1614327534.769173     >>>>> jest to czas unixowy
+time.struct_time(tm_year=2021, tm_mon=2, tm_mday=26, tm_hour=9, tm_min=18, tm_sec=54, tm_wday=4, tm_yday=57, tm_isdst=0)
+   February 2021
+Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+
+     March 2021
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31
+
+2020 is leap:  True
+                                  2021
+
+      January                   February                   March
+Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa
+                1  2          1  2  3  4  5  6          1  2  3  4  5  6
+ 3  4  5  6  7  8  9       7  8  9 10 11 12 13       7  8  9 10 11 12 13
+10 11 12 13 14 15 16      14 15 16 17 18 19 20      14 15 16 17 18 19 20
+17 18 19 20 21 22 23      21 22 23 24 25 26 27      21 22 23 24 25 26 27
+24 25 26 27 28 29 30      28                        28 29 30 31
+31
+
+       April                      May                       June
+Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa
+             1  2  3                         1             1  2  3  4  5
+ 4  5  6  7  8  9 10       2  3  4  5  6  7  8       6  7  8  9 10 11 12
+11 12 13 14 15 16 17       9 10 11 12 13 14 15      13 14 15 16 17 18 19
+18 19 20 21 22 23 24      16 17 18 19 20 21 22      20 21 22 23 24 25 26
+25 26 27 28 29 30         23 24 25 26 27 28 29      27 28 29 30
+                          30 31
+
+        July                     August                  September
+Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa
+             1  2  3       1  2  3  4  5  6  7                1  2  3  4
+ 4  5  6  7  8  9 10       8  9 10 11 12 13 14       5  6  7  8  9 10 11
+11 12 13 14 15 16 17      15 16 17 18 19 20 21      12 13 14 15 16 17 18
+18 19 20 21 22 23 24      22 23 24 25 26 27 28      19 20 21 22 23 24 25
+25 26 27 28 29 30 31      29 30 31                  26 27 28 29 30
+
+      October                   November                  December
+Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa      Su Mo Tu We Th Fr Sa
+                1  2          1  2  3  4  5  6                1  2  3  4
+ 3  4  5  6  7  8  9       7  8  9 10 11 12 13       5  6  7  8  9 10 11
+10 11 12 13 14 15 16      14 15 16 17 18 19 20      12 13 14 15 16 17 18
+17 18 19 20 21 22 23      21 22 23 24 25 26 27      19 20 21 22 23 24 25
+24 25 26 27 28 29 30      28 29 30                  26 27 28 29 30 31
+31
+
+>>>
+import datetime
+
+print("Minimalna wartośc i maksymalna: ", datetime.MINYEAR, datetime.MAXYEAR)
+
+dl = datetime.timedelta(days=1, hours=2, minutes=-30)
+print(dl)
+
+dl2 = datetime.timedelta(days=-1, hours=-2, minutes=-3)
+
+print(dl2)
+
+print("sumę timedelta: ", dl+dl2)
+
+print("Dziasiaj jest: ", datetime.date.today())
+
+>>>
+Minimalna wartośc i maksymalna:  1 9999
+1 day, 1:30:00
+-2 days, 21:57:00
+sumę timedelta:  -1 day, 23:27:00
+Dziasiaj jest:  2021-02-26
+>>> 
