1b51745c38cc389ed6a0155938e4adf42892b387
Natalia <57856955+korzeniowska18@users.noreply.github.com>
2021-02-27 08:06:01 +0100

    update

--- My_Fun_with_Python/rysowanie.py
+++ My_Fun_with_Python/rysowanie.py
@@ -33,4 +33,43 @@ def PrintBat():
     
 PrintBat()
 
+---------------
 
+def PrintCat():
+    # this function prints a cat ascii-art
+    txt = r'''
+|\---/|
+| o_o |
+ \_^_/'''
+    print(txt)
+    return
+ 
+def PrintBear():
+    # this function prints a bear ascii-art
+    txt = r'''
+/  \.-"""-./  \
+\    -   -    /
+ |   o   o   |
+ \  .-'"'-.  /
+  '-\__Y__/-'
+     `---`'''
+    print(txt)
+    return
+ 
+def PrintBat():
+    # this function prints a bat ascii-art
+    txt = r'''
+   /\                 /\
+  / \'._   (\_/)   _.'/ \
+ /_.''._'--('.')--'_.''._\
+ | \_ / `;=/ " \=;` \ _/ |
+  \/ `\__|`\___/`|__/`  \/
+          \(/|\)/  
+     '''
+    print(txt)
+    return
+ 
+ 
+PrintCat()
+PrintBear()
+PrintBat()
